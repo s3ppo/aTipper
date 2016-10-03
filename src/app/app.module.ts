776204f8f2aadc584a13408abd0bdd3e9ea966b2
Harald Wiesinger <Harald Wiesinger>
2016-10-03 14:29:46 +0200

    added first view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MaterialModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+  
+}
