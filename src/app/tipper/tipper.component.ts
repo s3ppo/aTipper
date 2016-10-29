1874139d1d8c2090bc58577eccc14d9f4cdb1b03
Harald Wiesinger <harald@moniholz.at>
2016-10-30 00:32:23 +0200

    Erstfassung

--- src/app/tipper/tipper.component.ts
+++ src/app/tipper/tipper.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
+import { DomSanitizer } from '@angular/platform-browser';
 
 import { MatchesService } from '../services/matches.service'
 import { MatchesModel } from '../models/matches';
@@ -16,17 +17,22 @@ export class TipperComponent implements OnInit{
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private matchesservice: MatchesService
+    private matchesservice: MatchesService,
+    private _DomSanitizationService: DomSanitizer,
   ){}
 
   private matchesmodelview: MatchesModel[];
+  private categoryname: string;
+  private numbermatches: string;
 
   ngOnInit(): void {
     this.route.params.forEach((params: Params) => {
       let category = params['category'];
       this.matchesservice.getAll(category)
                      .subscribe(
-                            matches => { this.matchesmodelview = matches },
+                            matches => { this.matchesmodelview = matches;
+                                         this.categoryname = matches[0]['category']['name'];
+                                         this.numbermatches = matches.length.toString(); },
                             err =>   { console.log(err) });
     });
   }
